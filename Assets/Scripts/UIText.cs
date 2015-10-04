25145bb2aeab1a8f4c86f02d00917aa6861d5f14
bthomas2b <sean72b@gmail.com>
2015-10-04 16:32:01 -0400

    PowerUps and some basic UI
    "Charge" - increases player boost charge by value (space to use)
    "BoostPad" - instant speed boost equal to value upon going over

---
+++ Assets/Scripts/UIText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIText : MonoBehaviour {
+
+    public PlayerController player;
+    public string variable;
+
+    private string initialText;
+    private Text text;
+
+	void Start () {
+        text = GetComponent<Text>();
+        initialText = text.text;
+	}
+	
+	void FixedUpdate () {
+        text.text = initialText + player.GetType().GetField(variable).GetValue(player);	
+        
+	}
+}
