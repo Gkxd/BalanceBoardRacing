6cc399f3b4ad5e889884f88d18ef1ca69454a313
Gkxd <dhua@andrew.cmu.edu>
2015-10-06 17:50:20 -0400

    Animated track arrows

---
+++ Assets/Scripts/Decoration/AnimateRampTexture.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateRampTexture : MonoBehaviour {
+
+    [Header("Reference Settings")]
+    public MeshRenderer renderer;
+
+    [Header("Gameplay Settings")]
+    public float stepSize;
+    public float speed;
+
+    void Update() {
+        float offset = speed * stepSize * Time.time;
+        renderer.material.SetTextureOffset("_MainTex", Vector2.right * offset);
+    }
+}
