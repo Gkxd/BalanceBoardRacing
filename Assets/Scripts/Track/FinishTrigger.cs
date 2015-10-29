640abb08d23fca5bac28b695273f28ca250382ff
tribunus.vii <tribunus.vii@gmail.com>
2015-10-29 01:16:25 -0700

    Finish line and timer

---
+++ Assets/Scripts/Track/FinishTrigger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FinishTrigger : MonoBehaviour {
+
+    private bool finished = false;
+
+    public float raceTime;
+    public int lapNumber;
+    public int maxLaps;
+
+    //public Text timer;
+
+    void OnTriggerEnter()
+    {
+        if (lapNumber < maxLaps)
+        {
+            lapNumber++;
+        }
+        else
+        {
+            Debug.Log("YOU WIN!");
+            finished = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        if (!finished) { 
+
+            //timer.text = raceTime.ToString("f2");
+
+            raceTime = raceTime + 1 * Time.deltaTime;
+        }
+    }
+}
